449f882ac9f7257c7790e04977107fd718e069a3
Tess Olson <37564977+eleanorpixie2@users.noreply.github.com>
2019-03-16 22:06:55 -0500

    Got NavMesh and Touch working

---
+++ Assets/Scripts/NavMeshMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMove : MonoBehaviour
+{
+    public Transform goal;
+    NavMeshAgent agent;
+    public float speed;
+    private bool reachedGoal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        goal.position = new Vector3(goal.position.x, transform.position.y, goal.transform.position.z);
+        agent.destination = goal.position;
+        reachedGoal = false;
+        //agent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (agent.speed <= speed && !reachedGoal)
+        {
+            agent.speed += agent.acceleration;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Rail"&&!reachedGoal)
+        {
+            agent.speed = 0;
+        }
+        else if(other.tag=="Goal")
+        {
+            agent.speed = 0;
+            reachedGoal = true;
+            agent.transform.position = new Vector3(other.transform.position.x-3, agent.transform.position.y, other.transform.position.z-3);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Touch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Touch : MonoBehaviour
+{
+    Quaternion startingRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.rotation=Quaternion.Euler(0,-120,0);
+            other.GetComponent<NavMeshAgent>().speed -= 5;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            Debug.Log("Pushing");
+            transform.rotation = Quaternion.Euler(0, -120, 0);
+            other.GetComponent<NavMeshAgent>().speed -= 5;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.rotation = startingRotation;
+        }
+    }
+}
