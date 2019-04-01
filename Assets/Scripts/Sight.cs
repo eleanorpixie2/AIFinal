b5c5b8bf239927db9ec3548d4b1f36fe1e340917
tess.olson <tess.olson@T3600-CRUSHER>
2019-04-01 15:18:51 -0500

    Edited the sight script to make the player move more adaptively

--- Assets/Scripts/Sight.cs
+++ Assets/Scripts/Sight.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,54 +9,104 @@ public class Sight : MonoBehaviour
     bool needsToGetAround;
     Vector3 intialSpeed;
 
+    public enum GetAroundFSM { MoveRight,MoveLeft,ContinueDriving};
+    private GetAroundFSM currentState;
+
     // Start is called before the first frame update
     void Start()
     {
         rgd = GetComponent<Rigidbody>();
-        needsToGetAround = false;
         intialSpeed = rgd.velocity;
+        currentState = GetAroundFSM.ContinueDriving;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //fwd vector
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        //raycast result
         RaycastHit hit;
-        Debug.DrawRay(transform.position, fwd, Color.red);
+        Debug.DrawRay(transform.position, fwd, Color.red,.5f,true);
+        //do a raycast
         if (Physics.Raycast(transform.position, fwd, out hit,10))
         { 
             print("move");
+            //if player then move
             if (hit.collider.tag == "Player")
-                needsToGetAround = true;
+                DetermineDirection(hit.collider.gameObject);
+            //otherwise ignore
             else
             {
-                needsToGetAround = false;
                 if (intialSpeed != rgd.velocity)
                 {
                     rgd.velocity = intialSpeed;
                 }
+                if (currentState != GetAroundFSM.ContinueDriving)
+                    currentState = GetAroundFSM.ContinueDriving;
             }
         }
+        //if there is nothing in front of the car
         else
         {
-            if (needsToGetAround)
-                needsToGetAround = false;
+            if (currentState != GetAroundFSM.ContinueDriving)
+                currentState = GetAroundFSM.ContinueDriving;
+        }
+        
+        //If the car is just driving
+        if(currentState==GetAroundFSM.ContinueDriving)
+        {
             if (intialSpeed != rgd.velocity)
             {
                 rgd.velocity = intialSpeed;
             }
         }
 
-        if (needsToGetAround)
+        //If the car needs to move to the right
+        if (currentState==GetAroundFSM.MoveRight)
         {
-            GetAroundOtherCar();
+            GetAroundOtherCarRight();
+        }
+        //If the car needs to move to the left
+        if(currentState == GetAroundFSM.MoveLeft)
+        {
+            GetAroundOtherCarLeft();
         }
 
     }
 
-    void GetAroundOtherCar()
+    //determine which way to move
+    void DetermineDirection(GameObject other)
     {
-        rgd.velocity += new Vector3(0,0,-2);
+        float direction = Math.Abs(other.gameObject.transform.position.z) - Math.Abs(transform.position.z);
+        if (direction > 0)
+            currentState = GetAroundFSM.MoveLeft;
+        else if (direction < 0)
+            currentState = GetAroundFSM.MoveRight;
     }
 
+    //move to the right
+    void GetAroundOtherCarRight()
+    {
+        rgd.velocity += new Vector3(1,0,2);
+    }
+    //move to the left
+    void GetAroundOtherCarLeft()
+    {
+        rgd.velocity += new Vector3(1, 0, -2);
+    }
+
+    //if hitting the rail then stop and move back and away from the railing
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Rail")
+        {
+            currentState = GetAroundFSM.ContinueDriving;
+            float direction = Math.Abs(other.gameObject.transform.position.z) - Math.Abs(transform.position.z);
+            if (direction < 0)
+                rgd.velocity += new Vector3(-2, 0, 2);
+            if (direction > 0)
+                rgd.velocity += new Vector3(-2, 0, -2);
+        }
+    }
 }
